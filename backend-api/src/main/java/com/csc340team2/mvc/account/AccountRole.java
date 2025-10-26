917ba4e62e683175a4ee399fdc05206ed9ac48de
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2025-10-26 16:45:59 -0400

    Add account classes

---
+++ backend-api/src/main/java/com/csc340team2/mvc/account/AccountRole.java
@@ -0,0 +1,7 @@
+package com.csc340team2.mvc.account;
+
+public enum AccountRole {
+    ADMIN,
+    CUSTOMER,
+    COACH
+}
---
+++ backend-api/src/main/java/com/csc340team2/mvc/account/AccountService.java
@@ -0,0 +1,19 @@
+﻿package com.csc340team2.mvc.account;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AccountService {
+    @Autowired
+    private AccountRepository accountRepository;
+    public Optional<Account> getAccountForEmail(String email){
+        return accountRepository.findByEmail(email);
+    }
+    public List<Account> getAllAccounts(){
+        return accountRepository.getAllBy();
+    }
+}
