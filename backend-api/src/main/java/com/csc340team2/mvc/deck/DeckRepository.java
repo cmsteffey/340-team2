25f4889392cf1488c09b615ed90621178187d074
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2025-10-26 17:02:59 -0400

    Add my deck files from ssteffey branch

---
+++ backend-api/src/main/java/com/csc340team2/mvc/deck/DeckRepository.java
@@ -0,0 +1,18 @@
+package com.csc340team2.mvc.deck;
+
+import com.csc340team2.mvc.account.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+
+public interface DeckRepository extends JpaRepository<Deck, Long>{
+    @Query("SELECT d FROM Deck d JOIN FETCH d.account account")
+    public List<Deck> getAllWithUsers();
+    public List<Deck> getAllBy();
+    public List<Deck> getAllByAccount(Account account);
+    @Modifying
+    public int deleteDecksById(Long id);
+}
---
+++ backend-api/src/main/java/com/csc340team2/mvc/deck/DeckService.java
@@ -0,0 +1,37 @@
+﻿package com.csc340team2.mvc.deck;
+
+import com.csc340team2.mvc.account.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeckService {
+    @Autowired
+    private DeckRepository deckRepository;
+
+    /*@Query("SELECT d FROM Deck d JOIN FETCH d.account account")
+    public List<Deck> getAllWithUsers();
+    public List<Deck> getAllBy();
+    public List<Deck> getAllByAccount(Account account);
+    @Modifying
+    public int deleteDecksById(Long id);
+    * */
+
+    public List<Deck> getAllDecksWithUsers(){
+        return deckRepository.getAllWithUsers();
+    }
+
+    public List<Deck> getAllDecksWithoutUsers(){
+        return deckRepository.getAllBy();
+    }
+
+    public List<Deck> getAllOwnedBy(Account account){
+        return deckRepository.getAllByAccount(account);
+    }
+
+    public boolean deleteDeckById(Long id){
+        return deckRepository.deleteDecksById(id) == 1;
+    }
+}
