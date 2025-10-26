c0caf54c1f54e4dd43e6b5bcc8431eaa135b8d82
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2025-10-26 17:17:33 -0400

    Add my appointment files from ssteffey branch

---
+++ backend-api/src/main/java/com/csc340team2/mvc/appointment/Appointment.java
@@ -0,0 +1,67 @@
+package com.csc340team2.mvc.appointment;
+
+import com.csc340team2.mvc.account.Account;
+import jakarta.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "appointment")
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Date time;
+
+    @Column(nullable = false)
+    private int length;
+
+    @JoinColumn(name = "customer_id", nullable = false)
+    @ManyToOne(targetEntity = Account.class)
+    private Account customer;
+
+    @JoinColumn(name = "coach_id", nullable = false)
+    @ManyToOne(targetEntity = Account.class)
+    private Account coach;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public Account getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Account customer) {
+        this.customer = customer;
+    }
+
+    public Account getCoach() {
+        return coach;
+    }
+
+    public void setCoach(Account coach) {
+        this.coach = coach;
+    }
+}
---
+++ backend-api/src/main/java/com/csc340team2/mvc/appointment/AppointmentRepository.java
@@ -0,0 +1,11 @@
+package com.csc340team2.mvc.appointment;
+
+import com.csc340team2.mvc.account.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    List<Appointment> getAppointmentsByCoach(Account coach);
+    List<Appointment> getAppointmentsByCustomer(Account customer);
+}
