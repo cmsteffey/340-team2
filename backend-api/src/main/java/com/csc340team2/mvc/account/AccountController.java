1d35d568e5c07c4702591e2944d4f95fc4f750c0
cmsteffey <65865777+cmsteffey@users.noreply.github.com>
2025-10-27 01:27:26 -0400

    Split API Controller into per-entity RestController

---
+++ backend-api/src/main/java/com/csc340team2/mvc/account/AccountController.java
@@ -0,0 +1,29 @@
+﻿package com.csc340team2.mvc.account;
+
+import com.csc340team2.mvc.session.Session;
+import com.csc340team2.mvc.session.SessionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class AccountController {
+    @Autowired
+    private AccountService accountService;
+    @Autowired
+    private SessionService sessionService;
+    @PostMapping("/registration")
+    public ResponseEntity registerAccount(@RequestBody Account account){
+        Account saved = accountService.addAccount(account);
+        Session createdSession = sessionService.createSessionForAccount(account);
+        return ResponseEntity.status(200).header("Set-Cookie", createdSession.getSetCookieHeader()).body(saved);
+    }
+    @GetMapping("/accounts/{id}")
+    public ResponseEntity getAccount(@PathVariable Long id) {
+        return ResponseEntity.ok(accountService.getAccountById(id));
+    }
+    @GetMapping("/accounts")
+    public ResponseEntity getAccounts(){
+        return ResponseEntity.ok(accountService.getAllAccounts());
+    }
+}
---
+++ backend-api/src/main/java/com/csc340team2/mvc/appointment/AppointmentController.java
@@ -0,0 +1,17 @@
+﻿package com.csc340team2.mvc.appointment;
+
+import com.csc340team2.mvc.session.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppointmentController {
+    @Autowired
+    private AppointmentService appointmentService;
+    @GetMapping("/appointments")
+    public ResponseEntity getAppointments(Session session){
+        return ResponseEntity.ok(appointmentService.getAppointmentsByCoach(session.getAccount()));
+    }
+}
